3b595e38f1c39f977e47f125768d06ccb8b8770e
karen <79118416+Karenjakins@users.noreply.github.com>
2021-03-20 10:04:09 -0400

    Election progress
    Python code - Module 3

---
+++ .vscode/ PyPoll.py
@@ -0,0 +1,33 @@
+# The data we need to retrieve
+# 1. Total number of votes cast
+# 2. A complete list of candidates who received votes
+# 3. Total number of votes each candidate received
+# 4. Percentage of votes each candidate won
+# 5. The winner of the election based on popular vote
+
+# Add our dependencies
+import csv
+import os
+# Assign a variable for the file to load and the path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("Analysis", "election_analysis.txt")
+
+# Open the elections results and read the file.
+with open(file_to_load) as election_data:
+    file_reader = csv.reader(election_data)
+
+    # Read and print the header row.
+    headers = next(file_reader)
+    print(headers)
+
+
+# Using the with statement open the file as a text file.
+with open(file_to_save, "w") as txt_file:
+
+    # Write three counties to the file.
+    txt_file.write("Counties in the Election\n")
+    txt_file.write("------------------------\n")
+    txt_file.write("Arapahoe\nDenver\nJefferson\n")
+
+
---
+++ Python_practice.py
@@ -0,0 +1,12 @@
+numbers = [0, 1, 2, 3, 4, 5]
+for num in range(5):
+    print(num)
+
+    # Import the datetime class from the datetime module
+import datetime as dt
+# Use the now () attribute on the datetime class to get the present time.
+now = datetime.datetime.now()
+# Prin the present time.
+print("The time right now is", now)
+import csv
+dir(csv)
